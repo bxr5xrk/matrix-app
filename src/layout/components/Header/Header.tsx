c2d71675950f89985a1538e7aafb588e80572ac2
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-03-07 15:46:09 +0200

    init

---
+++ src/layout/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import cl from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <header className={cl.root}>
+      <h1>Matrix App</h1>
+    </header>
+  );
+}
---
+++ src/layout/index.tsx
@@ -0,0 +1,12 @@
+import MainPage from '../pages/MainPage';
+import Header from './components/Header/Header';
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+
+      <MainPage />
+    </>
+  );
+}
